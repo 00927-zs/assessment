ed0368b69ce71989c4f3745c356a84e3a4edc0a2
r <257zs00927@student.zen.ac.jp>
2025-11-24 18:20:18 +0900

    Add files via upload

--- assessment.js
+++ assessment.js
@@ -16,14 +16,28 @@ assessmentButton.addEventListener
     }
     //診断エリアの作成
     resultDivision.innerText = '';
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivision.appendChild(header);
+
+    //headerDivision
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    //bodyDivision
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    //resultDivision ni bootstrap
+    resultDivision.setAttribute('class', 'card');
+
+    //headerDivision to bodyDivision wo resultDivision ni
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
 
     //ツイートエリアの作成
     tweetDivision.innerText = '';
